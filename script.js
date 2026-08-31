df5e0edeb2fa77be9ab3f3b39ff92b3003eb5d93
tazd898-ship-it <tazd898@gmail.com>
2026-08-31 14:05:11 +0100

    Add navigation interaction

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelector('.menu')?.addEventListener('click',()=>document.querySelector('nav').classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>document.querySelector('nav').classList.remove('open')));
